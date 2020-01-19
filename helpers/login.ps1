8253b17b66f52b8d79943a5a31d4b9ce25a7cbe3
Preet X Sangha <preet.sangha@serko.com>
2020-01-19 15:21:04 +1300

    updated script

--- helpers/login.ps1
+++ helpers/login.ps1
@@ -1,19 +1,35 @@
-Write-Output "login to Azure"
+$bigBangSub = '70e79a08-a3f2-4656-9b89-1d263835ba25';
 
-Get-ChildItem Env: | Sort-Object Name
+$imageRoot = "ImageGenerationRepositoryRoot"
 
-$password = $env:servicePrincipalKey | ConvertTo-SecureString -asPlainText -Force
+#$preetMpnSub = "c0c577e0-1dee-42d3-bf3c-c3124f7a948c";
+$resourceGroup = "preet-test-rg" ;
+$imageType = 1;
+$location = "australiaeast";
+$SecondsToWaitForServicePrincipalSetup = 1;
+$SubscriptionId = $bigBangSub
 
-$credential = New-Object -TypeName System.Management.Automation.PSCredential($env:servicePrincipalId,$password)  -ErrorAction Stop
-
-Write-Output "Credential created, installing Azure RM"
-
-
+Write-Output "Installing Azure RM"
 Install-Module AzureRM -Force -AllowClobber #-Verbose
 Import-Module AzureRM
 
-Write-Output "Installed Azure RM, logging in"
+" ---- "
+Get-ChildItem Env: | Sort-Object Name
+" ---- "
 
+Write-Output "Create Service Credential"
+$password = $env:servicePrincipalKey | ConvertTo-SecureString -asPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential($env:servicePrincipalId,$password)  -ErrorAction Stop
+
+Write-Output "Logging in"
 Login-AzureRmAccount -Credential $Credential -TenantId $env:tenantId -ServicePrincipal
 
-Write-Output "Logged In"
+Write-Output "Setting Subscription"
+Set-AzureRmContext -SubscriptionId $SubscriptionId
+
+Write-Output "Getting Service Principal"
+$sp = Get-AzureRmADServicePrincipal -ServicePrincipalName $env:servicePrincipalId
+$spAppId = $sp.ApplicationI
+$spClientId = $sp.ApplicationId
+$spObjectId = $sp.Id
+Write-Output "Service Principal: " $spAppId $spClientId $spObjectId
