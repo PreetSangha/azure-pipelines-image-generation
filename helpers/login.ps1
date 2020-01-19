c3d21265beb78bec6f6b53fe7cf7ebe605cb0959
Preet X Sangha <preet.sangha@serko.com>
2020-01-19 14:37:03 +1300

    updated script

--- helpers/login.ps1
+++ helpers/login.ps1
@@ -3,7 +3,8 @@ Write-Output "login to Azure"
 Get-ChildItem Env: | Sort-Object Name
 
 Install-Module AzureAutomationAuthoringToolkit -Scope CurrentUser -Force
-$Credential = Get-AutomationPSCredential -Name $env:USERNAME
+
+$Credential = Get-AutomationPSCredential -Name $env:servicePrincipalId
 
 $Credential | Get-Member
 
